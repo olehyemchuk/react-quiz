559e43e85a616707a977591e2b0ffa9123c98db6
Oleh <emchuk86@gmail.com>
2020-02-11 17:46:22 +0200

    initial

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import classes from './Drawer.module.css'
+import {NavLink} from 'react-router-dom'
+import Backdrop from "../../UI/Backdrop/Backdrop";
+
+const links = [
+  {to: '/', label: 'List', exact: true},
+  {to: '/auth', label: 'Authentication', exact: false},
+  {to: '/quiz-creater', label: 'Create test', exact: false},
+]
+
+class Drawer extends Component {
+
+  clickHandler = () => {
+    this.props.onClose()
+  }
+
+
+  renderLinks() {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    const cls = [classes.Drawer]
+
+    if (!this.props.isOpen) {
+      cls.push(classes.close)
+    }
+
+    return (
+      <React.Fragment>
+        <nav className={cls.join(' ')}>
+          <ul>
+            {this.renderLinks()}
+          </ul>
+        </nav>
+        { this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null }
+      </React.Fragment>
+    );
+  }
+}
+
+export default Drawer;
---
+++ src/containers/QuizCreater/QuizCreater.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class QuizCreater extends Component {
+  render() {
+    return (
+      <div>
+        <h1>QuizCreater</h1>
+      </div>
+    );
+  }
+}
